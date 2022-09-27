9c1bf043265f79e876ad0aea2d68f26ae5cc9f28
Gurpreet Singh <gu07869prince@gmail.com>
2022-09-27 14:35:27 +0530

    Modifications in library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 // PACKAGES
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+
+// REDUX STORE
+import { store } from "./Store/Store/configureStore";
 
 // Components
 import { Roots } from "./Services/Routes";
@@ -11,6 +16,9 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Roots />
+    <Provider store={store}>
+      <ToastContainer autoClose={1500} />
+      <Roots />
+    </Provider>
   </React.StrictMode>
 );
