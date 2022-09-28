4f0068134d95096dfed2cfbab4d43e199de295c1
Gurpreetsingh4547 <107097944+Gurpreetsingh4547@users.noreply.github.com>
2022-09-28 15:25:16 +0530

    Update index.js

--- src/view/TrendingProducts/index.js
+++ src/view/TrendingProducts/index.js
@@ -6,7 +6,7 @@ import { TreandingProductList } from "../products/treandingProductList";
 const TrendingProducts = () => (
   <section className="bg-white py-8">
     <div className="container mx-auto flex items-center flex-wrap pt-4 pb-12">
-      <nav id="store" className="w-full z-30 top-0 px-6 py-1">
+      <nav id="store" className="w-full z-0 top-0 px-6 py-1">
         <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
           <a
             className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl "
